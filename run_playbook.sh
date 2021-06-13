36ee583735293093cb4c0c330353be6882b1a609
Dmitry Monakhov <d.a.monakhov@gmail.com>
2021-06-13 10:57:38 -0400

    Initial

---
+++ run_playbook.sh
@@ -0,0 +1,6 @@
+ansible-playbook \
+	configure-system.yaml \
+	-i inventories/vbox/hosts.yaml \
+	-e roles/common/vars/users.yml \
+	--vault-password-file roles/common/vars/ansible_pass.txt \
+	-vvv
